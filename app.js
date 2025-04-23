8e488313f260fbc686208dd3d8da0f94f00ad979
ashwith123 <ashwith9492@gmail.com>
2025-04-23 09:27:42 +0530

    samll changes

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const precautionsMap = {
